3956f34bc9ed05919148fb3e13616930f04e8188
Андрей Немытов <43293747+ProSkater12@users.noreply.github.com>
2020-04-03 21:35:37 +0500

    Initial commit

---
+++ content_scripts/background.js
@@ -0,0 +1,39 @@
+// Универсальная функция обратного вызова onclick.
+function genericOnClick(info, tab) {
+  console.log('item ' + info.menuItemId + ' was clicked');
+  console.log("info: " + JSON.stringify(info));
+  console.log("tab: " + JSON.stringify(tab));
+}
+
+// Создать один тестовый элемент для каждого типа контекста.
+var contexts = ["page","selection","link","editable","image","video",
+                "audio"];
+for (var i = 0; i < contexts.length; i++) {
+  var context = contexts[i];
+  var title = "Test '" + context + "' menu item";
+  var id = chrome.contextMenus.create({"title": title, "contexts":[context],
+                                       "onclick": genericOnClick});
+  console.log("'" + context + "' item:" + id);
+}
+
+// Создание родительского элемента и двух дочерних элементов.
+// Create a parent item and two children.
+var parent = chrome.contextMenus.create({"title": "Test parent item"});
+var child1 = chrome.contextMenus.create(
+  {"title": "Child 1", "parentId": parent, "onclick": genericOnClick});
+var child2 = chrome.contextMenus.create(
+  {"title": "Child 2", "parentId": parent, "onclick": genericOnClick});
+console.log("parent:" + parent + " child1:" + child1 + " child2:" + child2);
+
+
+// Create some radio items.
+function radioOnClick(info, tab) {
+  console.log("radio item " + info.menuItemId +
+              " was clicked (previous checked state was "  +
+              info.wasChecked + ")");
+}
+var radio1 = chrome.contextMenus.create({"title": "Radio 1", "type": "radio",
+                                         "onclick":radioOnClick});
+var radio2 = chrome.contextMenus.create({"title": "Radio 2", "type": "radio",
+                                         "onclick":radioOnClick});
+console.log("radio1:" + radio1 + " radio2:" + radio2);
